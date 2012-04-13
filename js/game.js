7139440ec19fb5739b5d495a597a32bcda7d7708
agannon <assaf.gannon@gmail.com>
2012-04-13 22:54:32 +0200

    initial

---
+++ js/game.js
@@ -0,0 +1,66 @@
+var MathGame = function(){
+	var timer = Number(0);
+	var maxTime;
+	var timerCB;
+	var operations = [];
+	var timerInterval;
+	this.getQuestion = function(){
+		var op = operations[Math.floor(Math.random()*operations.length)];
+		var ok = false;
+		do{
+			var x = Math.floor(Math.random()*100);
+			var y = Math.floor(Math.random()*100);
+			if(op == subtract && x<y){
+				ok = false;	
+			}else if(op == add && x+y>100){
+				ok = false;	
+			}else{
+				ok = true;	
+			}
+		}while(!ok);
+		var sign = getSign(op);
+		var question = ''+x+sign+y+'=';
+		return question;
+	}
+	
+	this.init = function(timerCallback, setMaxTime){
+		operations.push(add);
+		operations.push(subtract);
+		timerCB = timerCallback;
+		maxTime = setMaxTime || 60;
+	}
+	
+	this.start = function(){
+		timerInterval = window.setInterval(timerUpdate, 1000);
+	}
+	
+	function timerUpdate(){
+		timer++;
+		if(timer>maxTime){
+			window.clearInterval(timerInterval);
+			alert("Time's up!");	
+		}else{
+			timerCB();	
+		}
+	}
+	
+	this.getTimer = function(){
+		return timer;	 
+	}
+	
+	function getSign(operation){
+		if(operation == add){
+			return '+';
+		}
+		if(operation == subtract){
+			return '-';	
+		}
+	}
+	function add(a,b){
+		return a+b;	
+	}
+	function subtract(a,b){
+		return a-b;	
+	}
+	
+}
